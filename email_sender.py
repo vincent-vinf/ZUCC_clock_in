0c8e00ab39d1900f705738a04d7fcc3b4ad49120
dparticle <dparticle@163.com>
2022-01-16 14:58:27 +0800

    添加假期地址可选项和通知每个打卡人功能

---
+++ email_sender.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+from email.mime.text import MIMEText
+from email.header import Header
+from smtplib import SMTP_SSL
+
+class EmailSender:
+    def __init__(self, host, username, password):
+        self.__username = username
+        # ssl login
+        self.__smtp = SMTP_SSL(host)
+        # set_debuglevel() for debug, 1 enable debug, 0 for disable
+        # smtp.set_debuglevel(1)
+        self.__smtp.ehlo(host)
+        self.__smtp.login(username, password)
+
+    def send(self, receiver, mail_title='', mail_content=''):
+        # construct message
+        msg = MIMEText(mail_content, "plain", 'utf-8')
+        msg["Subject"] = Header(mail_title, 'utf-8')
+        msg["From"] = self.__username
+        msg["To"] = receiver
+        self.__smtp.sendmail(self.__username, receiver, msg.as_string())
+
+    def quit(self):
+        self.__smtp.quit()
--- main.py
+++ main.py
@@ -5,10 +5,7 @@ import json
 import time
 import random
 from urllib import parse
-
-from email.mime.text import MIMEText
-from email.header import Header
-from smtplib import SMTP_SSL
+from email_sender import EmailSender
 
 auth_url = "http://ca.zucc.edu.cn/cas/login"
 clock_in_url = "http://ca.zucc.edu.cn/cas/login?service=http://yqdj.zucc.edu.cn/feiyan_api/h5/html/daka/daka.html"
@@ -28,6 +25,7 @@ post_data = {
 }
 # examen schema id
 examen_schema_id = 2
+examen_schema_validation = False
 
 
 # 自定义打卡失败异常类
@@ -60,23 +58,30 @@ def clock_in(config):
         # "_":""
     }
 
-    find_schema_url = find_examen_schema_url + "?" + parse.urlencode(param)
-    response = session.post(find_schema_url, data={"esId": examen_schema_id})
-    re = json.loads(response.text)
-    schema_local_path = "examen_schema.json"
-    if os.path.exists(schema_local_path):
-        f = open("examen_schema.json", 'r')
-        # scheme_local = json.loads(f.read())["scheme"]
-        update_time_local = json.loads(f.read())["scheme"]
-        # 判断 schema 更新时间是否相同
-        if update_time_local != re["data"]["examen"]["scheme"]:
-            # save_schema(re)
-            raise ClockInError("问卷内容更新")
-    else:
-        save_schema(re)
+    # 验证表单内容是否更新
+    global examen_schema_validation
+    if not examen_schema_validation:
+        find_schema_url = find_examen_schema_url + "?" + parse.urlencode(param)
+        response = session.post(find_schema_url, data={"esId": examen_schema_id})
+        re = json.loads(response.text)
+        schema_local_path = "examen_schema.json"
+        if os.path.exists(schema_local_path):
+            f = open("examen_schema.json", 'r')
+            scheme_local = json.loads(f.read())["scheme"]
+            # update_time_local = json.loads(f.read())["updateTime"]
+            # 判断 schema 更新时间是否相同
+            if scheme_local != re["data"]["examen"]["scheme"]:
+                # save_schema(re)
+                raise ClockInError("问卷内容更新！请及时更新！")
+        else:
+            save_schema(re)
+        examen_schema_validation = True
+
     final_url = handle_clock_in_url + "?" + parse.urlencode(param)
     print("post data: " + final_url)
-    response = session.post(final_url, json=make_request(config["vaccine"]))
+    response = session.post(final_url, json=make_request(
+        config["vaccine"], config["address"] if "address" in config.keys() or config["address"] == "" else ""
+    ))
     try:
         re = json.loads(response.text)
         print(re)
@@ -97,7 +102,7 @@ def save_schema(re):
     f.close()
 
 
-def make_request(vaccine):
+def make_request(vaccine, address):
     # vaccine为编号
 
     vaccine_list = {"1": "已接种两针剂疫苗（科兴、生物等）第一针", "2": "已接种两针剂疫苗（科兴、生物等）第二针", "3": "已接种两针剂疫苗（科兴、生物等）加强针",
@@ -105,58 +110,50 @@ def make_request(vaccine):
 
     today = time.strftime("%Y-%m-%d", time.localtime(time.time()))
     return {'examenSchemeId': examen_schema_id, 'examenTitle': '师生报平安',
-            'answer': '{"填报日期(Date)":"' + today + '","自动定位(Automatic location)":"浙江省 杭州市 拱墅区","目前所在地":"校内 校内 校内",'
-                                                  '"近14天是否有与疫情中、高风险地区人员的接触史？(Did you contact any person(s) from medium '
-                                                  'or high risk area of the epidemic in the past 14 days?)":"否(NO)",'
-                                                  '"近14天是否有与疑似、确诊人员的接触史?(In the past 14 days, did you contact any '
-                                                  'COVID-19 suspected or confirmed person(s)?)":"否(NO)",'
-                                                  '"近21天是否有中高风险地区旅居史，近14天是否有中高风险地区所在区旅居史？( In the past 21 days, '
-                                                  'have you been to medium or high-risk area? In the past 14 days, '
-                                                  'have you been to any places close to medium or high risk area?)":"否('
-                                                  'NO)","现是否处于医学观察状态？(Are you under medical observation currently '
-                                                  '?)":"否(NO)","若处于医学观察状态，请填写隔离地点和隔离起始时间？(If yes, please indicate the '
-                                                  'place and the starting date.)":"","现是否处于居家隔离状态？(Are you in home '
-                                                  'quarantine now?)":"否(NO)","若处于居家隔离状态，请填写隔离地点和隔离起始时间？(If yes, '
-                                                  'please indicate the place and the starting date.)":"",'
-                                                  '"现身体状况，是否存在发热体温、寒战、咳嗽、胸闷以及呼吸困难等症状？( Do you have any symptoms such as '
-                                                  'fever, chills, cough, chest tightness, and difficulty '
-                                                  'breathing?)":"否(NO)","同住家属近14天是否有与疫情中、高风险地区人员或疑似、确诊人员的接触史？(Did your '
-                                                  'family member (s) living together contact any person(s) from medium '
-                                                  'or high risk area in the past 14 days?)":"否(NO)",'
-                                                  '"同住家属近14天是否到过疫情中、高风险地区？(Have any family members living together been '
-                                                  'to medium or high risk area in the past 14 days?)":"否(NO)",'
-                                                  '"同住家属现是否处于医学观察状态?(Are the family members living together under '
-                                                  'medical observation?)":"否(NO)","疫苗接种情况?(Vaccination '
-                                                  'status?)":"'
-                      + vaccine_list[vaccine] + '","今日申领学校所在地健康码的颜色? What\'s the color '
-                                                'of today\'s health code?":"绿码(Green code)",'
-                                                '"本人或家庭成员(包括其他亲密接触人员)是否有近28日入境或未来7日拟入境的情况?Have you or your family '
-                                                'members(including other close contact persons) entered China over the '
-                                                'past 28 days or plan to enter China in 7 days?":"否 No"}'}
-
-
-def send_mail(config, mail_title='', mail_content=''):
-    if config is None:
-        return
-        # ssl login
-    smtp = SMTP_SSL(config["host"])
-    # set_debuglevel() for debug, 1 enable debug, 0 for disable
-    # smtp.set_debuglevel(1)
-    smtp.ehlo(config["host"])
-    smtp.login(config["username"], config["password"])
-
-    # construct message
-    msg = MIMEText(mail_content, "plain", 'utf-8')
-    msg["Subject"] = Header(mail_title, 'utf-8')
-    msg["From"] = config["username"]
-    msg["To"] = config["receiver"]
-    smtp.sendmail(config["username"], config["receiver"], msg.as_string())
-    smtp.quit()
+            'answer': '{"填报日期(Date)":"' + today + '","自动定位(Automatic location)":"' +
+                      (address if address else '浙江省 杭州市 拱墅区') + '","目前所在地":"' +
+                      (address if address else '校内 校内 校内') +
+                      '",'
+                      '"近14天是否有与疫情中、高风险地区人员的接触史？(Did you contact any person(s) from medium '
+                      'or high risk area of the epidemic in the past 14 days?)":"否(NO)",'
+                      '"近14天是否有与疑似、确诊人员的接触史?(In the past 14 days, did you contact any '
+                      'COVID-19 suspected or confirmed person(s)?)":"否(NO)",'
+                      '"近21天是否有中高风险地区旅居史，近14天是否有中高风险地区所在区旅居史？( In the past 21 days, '
+                      'have you been to medium or high-risk area? In the past 14 days, '
+                      'have you been to any places close to medium or high risk area?)":"否('
+                      'NO)","现是否处于医学观察状态？(Are you under medical observation currently '
+                      '?)":"否(NO)","若处于医学观察状态，请填写隔离地点和隔离起始时间？(If yes, please indicate the '
+                      'place and the starting date.)":"","现是否处于居家隔离状态？(Are you in home '
+                      'quarantine now?)":"否(NO)","若处于居家隔离状态，请填写隔离地点和隔离起始时间？(If yes, '
+                      'please indicate the place and the starting date.)":"",'
+                      '"现身体状况，是否存在发热体温、寒战、咳嗽、胸闷以及呼吸困难等症状？( Do you have any symptoms such as '
+                      'fever, chills, cough, chest tightness, and difficulty '
+                      'breathing?)":"否(NO)","同住家属近14天是否有与疫情中、高风险地区人员或疑似、确诊人员的接触史？(Did your '
+                      'family member (s) living together contact any person(s) from medium '
+                      'or high risk area in the past 14 days?)":"否(NO)",'
+                      '"同住家属近14天是否到过疫情中、高风险地区？(Have any family members living together been '
+                      'to medium or high risk area in the past 14 days?)":"否(NO)",'
+                      '"同住家属现是否处于医学观察状态?(Are the family members living together under '
+                      'medical observation?)":"否(NO)","疫苗接种情况?(Vaccination '
+                      'status?)":"' + vaccine_list[vaccine] +
+                      '","今日申领学校所在地健康码的颜色? What\'s the color '
+                      'of today\'s health code?":"绿码(Green code)",'
+                      '"本人或家庭成员(包括其他亲密接触人员)是否有近28日入境或未来7日拟入境的情况?Have you or your family '
+                      'members(including other close contact persons) entered China over the '
+                      'past 28 days or plan to enter China in 7 days?":"否 No"}'}
 
 
 if __name__ == '__main__':
     with open("./config.json", 'r', encoding='utf-8') as configs:
         configs = json.load(configs)
+        email_config = configs["email"]
+        try:
+            sender = EmailSender(email_config["host"], email_config["username"], email_config["password"])
+        except BaseException as e:
+            log = "获取邮箱发送实例失败，" + e.__str__() + "！\n"
+            print(log)
+            exit(0)
+
         log = time.strftime("%Y-%m-%d", time.localtime(time.time())) + ":\n"
         for config in configs["user"]:
             u = config["username"]
@@ -164,13 +161,19 @@ if __name__ == '__main__':
                 u += "(" + config["tag"] + ")"
             try:
                 if clock_in(config):
-                    log += u + ": 打卡成功！\n"
+                    u += ": 打卡成功！\n"
                 else:
-                    log += u + ": 打卡失败！\n"
+                    u += ": 打卡失败！\n"
             except ClockInError as e:
-                log += u + ": 打卡失败，" + e.msg + "！\n"
+                log = e.msg
+                break
             except BaseException as e:
-                log += u + ": 打卡失败，未处理的异常!!!\n"
+                u += ": 打卡失败，未处理的异常！\n"
+            # 发送打卡信息给打卡人
+            if "email" in config.keys() and config["email"] != "":
+                sender.send(config["email"], "ZUCC 打卡日志", u)
+            log += u
 
         print(log)
-        send_mail(configs["email"], "ZUCC打卡日志", log)
+        sender.send(email_config["receiver"], "ZUCC 打卡日志（总）", log)
+        sender.quit()
